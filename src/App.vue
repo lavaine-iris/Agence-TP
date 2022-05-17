854cd06c5207c183b18b83e123716f0a81fc68ed
lavaine-iris <iris.lavaine@edu.univ-fcomte.fr>
2022-05-17 14:14:21 +0200

    Header responsive

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,85 @@
   <div>
     <header
       class="
-        flex
+        sticky
+        top-0
+        z-40
         h-16
         w-full
-        items-center
-        justify-between
         border-b border-b-indigo-100
         bg-gray-100
         px-3
       "
     >
-      <div class="flex items-center">
-        <HomeIcon class="m-2 w-8 fill-indigo-500" />
-        <span class="font-semibold">Estatery</span>
-      </div>
-      <nav class="absolute top-16 lg:relative lg:top-1">
-        <ul class="flex gap-8">
-          <li>Rent</li>
-          <li>Buy</li>
-          <li>Sell</li>
-          <li>Manage Property</li>
-          <li>Ressources</li>
-        </ul>
-      </nav>
-      <div>
-        <MenuIcon class="w-8" />
+      <div class="flex h-full w-full items-center justify-between">
+        <div class="flex items-center">
+          <HomeIcon class="w-8 fill-indigo-500" />
+          <span class="font-semibold">Estatery</span>
+        </div>
+        <div class="lg:hidden">
+          <MenuIcon class="w-8 cursor-pointer" @click="MenuOpen = !MenuOpen" />
+        </div>
+        <nav
+          class="
+            absolute
+            left-0
+            top-16
+            w-full
+            -translate-y-[200%]
+            bg-slate-300
+            pl-2
+            lg:relative lg:top-0 lg:w-fit lg:translate-y-0 lg:bg-transparent
+          "
+          :class="MenuOpen && 'translate-y-0'"
+        >
+          <ul class="flex flex-col gap-8 py-4 lg:flex-row">
+            <li>Rent</li>
+            <li>Buy</li>
+            <li>Sell</li>
+            <li>Manage Property</li>
+            <li>Ressources</li>
+          </ul>
+        </nav>
       </div>
     </header>
-    <main class="bg-slate-50">
-      <div class="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))]">
-        <h2 class="text-center text-4xl font-bold leading-10">
-          Search properties to rent
-        </h2>
-        <div class="flex items-center">
+    <main class="bg-gray-50 py-14 px-4 font-inter">
+      <div class="mx-auto mb-7 flex w-4/5 flex-col gap-7">
+        <div class="flex flex-col gap-7 lg:flex-row lg:justify-between">
+          <h2 class="text-center text-4xl font-bold text-indigo-900">
+            Search properties to rent
+          </h2>
+          <div class="flex items-center">
+            <input
+              type="search"
+              placeholder="Search with Search Bar"
+              class="
+                h-12
+                w-full
+                rounded-lg
+                border-2 border-indigo-100
+                px-4
+                placeholder:text-xs
+              "
+            />
+            <ChevronDownIcon
+              class="
+                -ml-8
+                h-5
+                w-5
+                rounded-full
+                bg-indigo-100
+                stroke-indigo-600
+                p-1
+              "
+            />
+          </div>
+        </div>
+        <div class="flex items-center lg:hidden">
           <input
             type="search"
-            placeholder="Search with Search Bar"
+            placeholder="Search Location"
             class="
-              h-12
+              h-16
               w-full
               rounded-lg
               border-2 border-indigo-100
@@ -47,82 +88,57 @@
               placeholder:text-xs
             "
           />
-          <ChevronDownIcon
+          <SearchIcon
             class="
-              -ml-8
-              h-5
-              w-5
-              rounded-full
-              bg-indigo-100
-              stroke-indigo-600
+              -ml-12
+              h-10
+              w-10
+              rounded-sm
+              bg-indigo-600
+              stroke-indigo-100
               p-1
             "
           />
         </div>
-      </div>
-      <div class="flex items-center lg:hidden">
-        <input
-          type="search"
-          placeholder="Search Location"
+        <div
           class="
-            h-16
+            hidden
+            h-24
             w-full
+            items-center
+            gap-4
             rounded-lg
             border-2 border-indigo-100
             px-4
-            placeholder:text-xs
+            font-inter
+            lg:flex
           "
-        />
-        <SearchIcon
-          class="
-            -ml-12
-            h-10
-            w-10
-            rounded-sm
-            bg-indigo-600
-            stroke-indigo-100
-            p-1
-          "
-        />
-      </div>
-      <div
-        class="
-          hidden
-          h-24
-          w-full
-          items-center
-          gap-4
-          rounded-lg
-          border-2 border-indigo-100
-          px-4
-          font-inter
-          lg:flex
-        "
-      >
-        <div class="w-1/2 min-w-max border-r-2 border-gray-300 px-4">
-          <span class="text-base text-gray-900">Location</span>
-          <p class="text-lg font-bold">New York, USA</p>
-        </div>
-        <div class="w-1/2 min-w-max border-r-2 border-gray-300 px-4">
-          <span class="text-base text-gray-900">When</span>
-          <p class="text-lg font-bold">Select Move-in Date</p>
-        </div>
-        <div class="w-1/2 min-w-max border-r-2 border-gray-300 px-4 pr-10">
-          <span class="text-base text-gray-900">Price</span>
-          <p class="text-lg font-bold">$500-$2,500</p>
-        </div>
-        <div class="flex w-1/2 items-center justify-center">
-          <p
-            class="
-              rounded-sm
-              bg-indigo-600
-              px-6
-              py-3
-              text-center text-indigo-100
-            "
-          >
-            Search
-          </p>
+        >
+          <div class="w-1/2 min-w-max border-r-2 border-gray-300 px-4">
+            <span class="text-sm text-gray-900">Location</span>
+            <p class="font-bold">New York, USA</p>
+          </div>
+          <div class="w-1/2 min-w-max border-r-2 border-gray-300 px-4">
+            <span class="text-sm text-gray-900">When</span>
+            <p class="font-bold">Select Move-in Date</p>
+          </div>
+          <div class="w-1/2 min-w-max border-r-2 border-gray-300 px-4 pr-10">
+            <span class="text-sm text-gray-900">Price</span>
+            <p class="font-bold">$500-$2,500</p>
+          </div>
+          <div class="flex w-1/2 items-center justify-center">
+            <p
+              class="
+                rounded-sm
+                bg-indigo-600
+                px-6
+                py-3
+                text-center text-indigo-100
+              "
+            >
+              Search
+            </p>
+          </div>
         </div>
       </div>
       <div
@@ -131,9 +147,8 @@
           grid
           w-4/5
           grid-cols-[repeat(auto-fit,minmax(300px,1fr))]
-          justify-between
+          justify-center
           gap-7
-          py-14
         "
       >
         <Card>3 Bathrooms</Card>
@@ -252,6 +267,11 @@ export default {
     Facebook,
     Instagram,
     Twitter,
+  },
+  data() {
+    return {
+      MenuOpen: false,
+    };
   },
 };
 </script>
